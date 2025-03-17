dd250beaffbd869d34b40e440310e412e56b94f2
takashi823 <uchimura.takashi724@gmail.com>
2025-03-17 12:09:21 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        quotes: 'double',
+      },
+    },
+  },
+})
